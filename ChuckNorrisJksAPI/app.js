ab140afa9e8b666d16b127198e5e8a0580944b38
Amir Velasquez <66282280+AmirJV1@users.noreply.github.com>
2021-06-25 15:29:19 -0400

    Add files via upload

---
+++ ChuckNorrisJksAPI/app.js
@@ -0,0 +1,32 @@
+const form = document.querySelector(".form1")
+const jokeDisplay = document.querySelector(".joke")
+
+async function getChnJk() {
+    try {
+        const params = {
+            headers: {
+                accept: 'application/json',
+                'x-rapidapi-key': 'ec902dcfe6msh7830455c2201bdep1c8b22jsnb372a33c149d',
+                'x-rapidapi-host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
+            }
+        };
+        const res = await axios.get("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random", params)
+        return res.data.value
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const showJoke = () => {
+    const timeout = 100;
+    setTimeout(async () => {
+        const joke = await getChnJk();
+        jokeDisplay.innerText = joke
+        console.log(joke)
+    }, timeout);
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    showJoke();
+})
